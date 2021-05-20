68d194399ee3c3808374ce7f1901714b85f0a02f
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-20 22:15:51 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
 import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent'
-import {MouseComponent} from './MouseComponent.js'
+import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
 	render() {
